20da35697bc62ba26667c118fca011aed00c4fa1
Sanne Stuur <sanne.stuur@gmail.com>
2018-02-15 16:35:36 +0100

    Changed languages to be compared to three

--- namen.rb
+++ namen.rb
@@ -2,33 +2,26 @@ french = ["pierre", "paul", "jean"]
 english = ["john", "paul", "peter"]
 dutch = ["peter",  "jan", "paul"]
 
+languages = [french, english, dutch]
+
+# @language_count = languages.count
+
 @candidates =[]
 
-# def compare(array1)
-#   array1.each {|item_from_array1|
-#     if item_from_array1 == paul
-#       candidate << item_from_array1
-#     end
+# languages.each_char {|language|
+#   language.each
 #   }
-# end
-#
-# compare(@french)
 
-def compare (array1, array2)
+def compare (array1, array2, array3)
   array1.each {|item|
-    if array2.include?(item) && !@candidates.include?(item)
+    if array2.include?(item) && array3.include?(item) && !@candidates.include?(item)
       @candidates << item
     end
   }
 end
 
-compare(french, english)
-compare(french, dutch)
-compare(english, dutch)
+compare(french, english, dutch)
 
 print @candidates
 
-# && !@candidates.include?(item)
-
-
-#try arrays of different lengths
+# print @language_count
